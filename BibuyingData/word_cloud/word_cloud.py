e5e0d0aea175087bc3f8eace2497ec8278e5b13b
cww97_mac <cww970329@qq.com>
2018-05-25 01:31:10 +0800

    whats woring

--- BibuyingData/word_cloud/word_cloud.py
+++ BibuyingData/word_cloud/word_cloud.py
@@ -7,36 +7,41 @@ from PIL import Image
 from bs4 import BeautifulSoup
 import requests
 from BibuyingData.main import get_artists
+from io import BytesIO
+import jieba
+import json
 
 d = path.dirname(__file__)
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) ''AppleWebKit/537.36 (KHTML, like Gecko) ' 'Chrome/51.0.2704.63 Safari/537.36'}
 
 
 def getWordCloud():
-    # get text && get fonts & maskimage
+    # get text && get fonts & mask_image
     text = open("dan.txt", "r", encoding='utf-8').read()
+    text = ' '.join(jieba.cut(text, cut_all=True))
     font = os.path.join(os.path.dirname(__file__), "../word_cloud/DroidSansFallbackFull.ttf")
     coloring = np.array(Image.open(path.join(d, "../word_cloud/bg.png")))
     
     # set stopwords
     stopwords = set(STOPWORDS)
-    ignorewords = ['蚊子的蚊子', '伟大的蚊子', '乔丹', '图片', '您好', '我现在有事不在',
+    ignore_words = ['哈', '哈哈', '哈哈哈', '哈哈哈哈','伟大', '蚊子', '乔丹', '图片', '您好',
+                    '我现在有事不在',
                    '一会再和您联系', '系统消息', '你撤回了一条消息','GLOW', '动作消息','表情',
                    '陈秋雨','google','https','maps','我在这里','点击查看',
                    '对方已成功接收了您发送的离线文件','zh','CN','曹艺舰','海鱼','culex',
                    'Believe','放手会冷','曹艺舰给您发送了一个窗口抖动','html',
                    '蚊子的蚊子给您发送了一个窗口抖动']
-    for item in ignorewords:
+    for item in ignore_words:
         stopwords.add(item)
     # Generate a word cloud image
-    wordcloud = WordCloud(background_color="white",font_path=font,stopwords=stopwords,
-                          mask=coloring, random_state=2,margin=1).generate(text)
+    wordcloud = WordCloud(background_color="white", font_path=font, stopwords=stopwords,
+                          mask=coloring, random_state=2, margin=1).generate(text)
     wordcloud = wordcloud.recolor(color_func=ImageColorGenerator(coloring))
     # Display the generated image, the matplotlib way:
     plt.imshow(wordcloud,interpolation='bilinear')
     plt.axis("off")
     plt.savefig("jordan2.png")
-    plt.show()
+    # plt.show()
     plt.close()
 
 
@@ -48,17 +53,47 @@ def get_soup(web_url):
     return soup
 
 
-def get_artist_name():
+def get_artist_info(cur_id: int, path_name: str):
+    soup = get_soup('http://music.163.com/artist?id=' + str(cur_id))
+    artist_name = soup.find('h2', attrs={'class': 'sname f-thide sname-max'}).string
+
+    # save image for each artist
+    img = requests.get(soup.find('div', attrs={'class': 'n-artist f-cb'}).img['src'])
+    image = Image.open(BytesIO(img.content))
+    image.save('%s%s.jpg' % (path_name, artist_name))
+
+    # save description for the artist
+    txt_file = open('%s%s.txt' % (path_name, artist_name), 'w', encoding='utf-8')
+    txt_file.write(soup.find(attrs={"name": "description"})['content'])
+
+    print(artist_name)
+
+
+def get_artists_info():
     artists_id = get_artists()
-    artists = []
+    path_name = os.path.dirname(os.getcwd()) + '/ArtistsData/'
+    cnt = 0
     for cur_id in artists_id:
-        soup = get_soup('http://music.163.com/artist?id=' + str(cur_id))
-        artists.append({'id': cur_id,
-                        'name': soup.find('h2', attrs={'class': 'sname f-thide sname-max'}).string,
-                        'description': soup.find(attrs={"name": "description"})['content']})
-        print(artists[-1])
-    print(artists)
+        get_artist_info(cur_id, path_name)
+        print('cnt = ' + str(cnt))
+        cnt += 1
+    print('done')
+
+
+def give_songs_to_artists():
+    songs_path = os.path.dirname(os.getcwd()) + '/SongsData/'
+    artists_path = os.path.dirname(os.getcwd()) + '/ArtistsData/'
+    song_files = os.listdir(songs_path)
+    for i in range(302, len(song_files)):
+        if song_files[i] == 'fuck.py': continue
+        with open("%s%s" % (songs_path, song_files[i]), encoding='utf-8') as f:
+            print('i = %d，%s' % (i, song_files[i]))
+            song = json.load(f)
+        # print(song['song_lyric'])
 
 
 if __name__ == "__main__":
-    getWordCloud()
+    # get_artists_info()
+    give_songs_to_artists()
+    # getWordCloud()
+    pass
